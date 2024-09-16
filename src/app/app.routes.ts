6a95daa8deea88c5abd474a25ebdd55714bdb702
fshippman <fshippman@yahoo.com>
2024-09-16 14:51:56 +0200

    added imprint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 export const routes: Routes = [
   {path: '', component: DashboardComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'user', component: UserComponent},
   {path: 'user/:id', component: UserDetailComponent},
+  {path: 'imprint', component: ImprintComponent}
 ];
  
